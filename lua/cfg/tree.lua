a28ac3cb5ef0149e484baa915cc40c791c96df31
jadnw <jadenwu137@protonmail.com>
2022-06-04 18:22:23 +0700

    :bug: Fix warning nvim-tree config

--- lua/cfg/tree.lua
+++ lua/cfg/tree.lua
@@ -1,49 +1,54 @@
 -- https://github.com/kyazdani42/nvim-tree.lua
-vim.g.nvim_tree_git_hl = 1 -- 0 by default, will enable file highlight for git attributes (can be used without the icons).
-vim.g.nvim_tree_highlight_opened_files = 1 --0 by default, will enable folder and file icon highlight for opened files/directories.
-vim.g.nvim_tree_root_folder_modifier = ":~" -- This is the default. See :help filename-modifiers for more options
-vim.g.nvim_tree_add_trailing = 0 -- 0 by default, append a trailing slash to folder names
-vim.g.nvim_tree_group_empty = 0 -- 0 by default, compact folders that only contain a single folder into one node in the file tree
-vim.g.nvim_tree_icon_padding = "  " -- one space by default, used for rendering the space between the icon and the filename. Use with caution, it could break rendering if you set an empty string depending on your font.
-vim.g.nvim_tree_symlink_arrow = " ➛  " -- defaults to " ➛ ". used as a separator between symlinks" source and target.
-vim.g.nvim_tree_respect_buf_cwd = 0 -- 0 by default, will change cwd of nvim-tree to that of new buffer"s when opening nvim-tree.
-vim.g.nvim_tree_create_in_closed_folder = 1 -- 0 by default, When creating files, sets the path of a file when cursor is on a closed folder to the parent folder when 0, and inside the folder when 1.
-vim.g.nvim_tree_special_files = { "README.md", "Makefile", "MAKEFILE" } -- List of filenames that gets highlighted with NvimTreeSpecialFile
-vim.g.nvim_tree_show_icons = {
-  git = 0,
-  folders = 1,
-  files = 1,
-  folder_arrows = 0,
-}
-vim.g.nvim_tree_icons = {
-  default = "",
-  symlink = "",
-  git = {
-    unstaged = "",
-    staged = "",
-    unmerged = "שׂ",
-    renamed = "凜",
-    untracked = "",
-    deleted = "ﮁ",
-    ignored = "ﴔ",
-  },
-  folder = {
-    default = "",
-    open = "ﱮ",
-    empty = "",
-    empty_open = "ﱮ",
-    symlink = "",
-    symlink_open = "ﱮ",
-  },
-}
-
 require("nvim-tree").setup({
   view = {
-    width = 30,
+    width = 36,
     hide_root_folder = false,
+    preserve_window_proportions = true,
+  },
+  renderer = {
+    group_empty = false,
+    highlight_git = true,
+    highlight_opened_files = "none",
+    icons = {
+      padding = "  ",
+      symlink_arrow = "  ",
+      show = {
+        file = true,
+        folder = true,
+        folder_arrow = false,
+        git = false,
+      },
+      glyphs = {
+        default = "",
+        symlink = "",
+        git = {
+          unstaged = "",
+          staged = "",
+          unmerged = "שׂ",
+          renamed = "凜",
+          untracked = "",
+          deleted = "ﮁ",
+          ignored = "ﴔ",
+        },
+        folder = {
+          default = "",
+          open = "ﱮ",
+          empty = "",
+          empty_open = "ﱮ",
+          symlink = "",
+          symlink_open = "ﱮ",
+        },
+      },
+    },
+    special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md" },
   },
   filters = {
     dotfiles = true,
     custom = { "__pycache__$", ".git$", "node_modules$" },
+  },
+  git = {
+    enable = true,
+    ignore = false,
+    timeout = 400,
   },
 })
