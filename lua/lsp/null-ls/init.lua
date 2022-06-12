8e6a1ed63cf58a919a6968e57d1fd0779a877ad8
jadnw <jadenwu137@protonmail.com>
2022-06-12 17:11:26 +0700

    :bug: Enable prettier & eslint for svelte [null-ls]

--- lua/lsp/null-ls/init.lua
+++ lua/lsp/null-ls/init.lua
@@ -12,7 +12,10 @@ local sources = {
   -- CODE ACTIONS
   null_ls.builtins.code_actions.gitsigns,
   -- DIAGNOSTICS
-  null_ls.builtins.diagnostics.eslint,
+  null_ls.builtins.diagnostics.eslint.with({
+    filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue", "svelte" },
+    command = "./node_modules/.bin/eslint",
+  }),
   null_ls.builtins.diagnostics.golangci_lint,
   null_ls.builtins.diagnostics.luacheck,
   -- null_ls.builtins.diagnostics.markdownlint,
@@ -25,7 +28,10 @@ local sources = {
   null_ls.builtins.formatting.fish_indent,
   null_ls.builtins.formatting.gofmt,
   -- null_ls.builtins.formatting.markdownlint,
-  null_ls.builtins.formatting.prettier,
+  null_ls.builtins.formatting.prettier.with({
+    filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue", "css", "scss", "less", "html", "json", "jsonc", "yaml", "markdown", "graphql", "handlebars", "svelte" },
+    command = "./node_modules/.bin/prettier",
+  }),
   null_ls.builtins.formatting.stylua,
 }
 
